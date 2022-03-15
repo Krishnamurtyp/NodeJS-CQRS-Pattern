d4882c3f6ca6b860cf51e6d5a8995885cc1752ba
Nguyen Linh Thanh Nhan <nguyenlinhthanhnhan@outlook.com>
2022-03-15 15:32:15 +0700

    Config Aggregators in src/config.js

--- src/config.js
+++ src/config.js
@@ -5,25 +5,46 @@ const createMessageStore = require('./message-store')
 const createHomeApp = require('./app/home')
 const createRecordViewingsApp = require('./app/record-viewings')
 
+const createHomePageAggregator = require('./aggregators/home-page')
+
 function createConfig({ env }) {
+    
+    // Get knex client
     const knexClient = createKnexClient({
         connectionString: env.databaseUrl
     })
+    
+    // Get postgres client
     const postgresClient = createPostgresClient({
         connectionString: env.messageStoreConnectionString
     })
+    
+    // Get messageStore
     const messageStore = createMessageStore({db: postgresClient})
 
 
     const homeApp = createHomeApp({ db:knexClient })
     const recordViewingsApp = createRecordViewingsApp({ messageStore })
 
+    const homePageAggregator = createHomePageAggregator({
+        db:knexClient,
+        messageStore
+    })
+    // Aggregators
+    const aggregators = [homePageAggregator]
+    
+    // Components
+    const components = []
+    
     return {
         env,
-        db,
+        db: knexClient,
         homeApp,
         messageStore,
-        recordViewingsApp
+        recordViewingsApp,
+        homePageAggregator,
+        aggregators,
+        components
     }
 }
 
