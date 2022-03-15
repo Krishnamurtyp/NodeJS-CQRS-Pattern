e25fb745d815a65c1790331d0a01559996baf10e
Nguyen Linh Thanh Nhan <nguyenlinhthanhnhan@outlook.com>
2022-03-15 15:00:41 +0700

    Create pages table

---
+++ migrations/20220315072013_create_pages.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('pages', tableBuilder => {
+      tableBuilder.string('page_name').primary()
+      tableBuilder.jsonb('page_data').defaultTo('{}')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('pages')
+};
