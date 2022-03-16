17471c2ee3df42f0e8bc5c01c469c4510f22706e
Nguyen Linh Thanh Nhan <nguyenlinhthanhnhan@outlook.com>
2022-03-16 14:56:24 +0700

    Add function return the category of the supplied 'streamname' in message-store/category.js

---
+++ src/message-store/category.js
@@ -0,0 +1,15 @@
+﻿/**
+ * @description Returns the category of the supplied `streamName`
+ * @param {string} streamName The streamName from which to extract the category
+ * @returns {string} The category of the supplied `streamName`
+ */
+function category (streamName) {
+    // Double equals to catch null and undefined
+    if (streamName == null) {
+        return ''
+    }
+
+    return streamName.split('-')[0]
+}
+
+module.exports = category
