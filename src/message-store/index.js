0dfb1911aa48777839d3fae81581762d824aff7a
Nguyen Linh Thanh Nhan <nguyenlinhthanhnhan@outlook.com>
2022-03-16 14:55:15 +0700

    Add read functions to the message store interface

--- src/message-store/index.js
+++ src/message-store/index.js
@@ -1,8 +1,22 @@
 ﻿const createWrite = require('./write')
+const createRead = require('./read')
+const configureCreateSubscription = require("./subscribe")
+
 function createMessageStore({db}) {
     const write = createWrite({db})
-    return {
+    
+    const read = createRead({db})
+    const createSubscription = configureCreateSubscription({
+        read:read.read,
+        readLastMessage: read.readLastMessage,
         write:write
+    })
+
+    return {
+        write:write,
+        createSubscription,
+        read: read.read,
+        readLastMessage: read.readLastMessage
     }
 }
 
